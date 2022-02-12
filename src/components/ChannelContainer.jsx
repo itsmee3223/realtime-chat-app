1742c44b7cae3a2541385e35f929caa6a643621c
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-12 21:12:05 +0700

    base application

---
+++ src/components/ChannelContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+function index() {
+  return <div>ChannelContainer</div>;
+}
+
+export default index;
